0e5986bb3764365f2f6250eb5cdbc5bd5c89c05f
Nesibe93 <125722258+Nesibe93@users.noreply.github.com>
2023-03-22 12:37:50 +0300

    ...

--- ADO01/ADO01_Detail.cs
+++ ADO01/ADO01_Detail.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -27,7 +28,7 @@ namespace ADO01
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            string vs_SQLTest;
+            string vs_SQLText = " ";
 
             // Bu form her iki işe de yarayacağı için
             // hangi modda olduğumu anlamam lazım
@@ -37,10 +38,43 @@ namespace ADO01
                 // daha sade bir komut satırı olması için
                 // parametrik kullanım tercih edildi
                 case "U":
-
+                    vs_SQLText = "UPDATE Customers SET";
+                  //  vs_SQLTest += "CompanyName = '" + txtCompanyName.Text + "'";
+                    vs_SQLText += "CompanyName = @CompanyName";
+                    vs_SQLText += "ContactName = @ContactName";
+                    vs_SQLText += "WHERE CustomerID = @CustomerID";
+                    // SQL text oluşturuldu..Parametreli
+                    break;
 
                 default:
                     break;
+            }
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                using (SqlCommand cmd = new SqlCommand(vs_SQLText, con))
+                {
+                    // Şimdi sıra parametreli doldurmakta
+                    cmd.Parameters.AddWithValue("CustomerID",
+                        txtCustomerID.Text);
+                    cmd.Parameters.AddWithValue("CompanyName",
+                        txtCompanyName.Text);
+                    cmd.Parameters.AddWithValue("ContactName",
+                        txtContactName.Text);
+                    cmd.Parameters.AddWithValue("Country",
+                        txtCountry.Text);
+
+                    cmd.CommandType = CommandType.Text;
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery(); // çalıştırıyor..Sql textimi sql server tarafına gönderiyor
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
             }
         }
     }
