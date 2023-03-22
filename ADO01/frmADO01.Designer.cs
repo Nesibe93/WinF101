2170ab96ffeb102744e459c75b63010eb5ed81d7
Nesibe93 <125722258+Nesibe93@users.noreply.github.com>
2023-03-22 09:52:56 +0300

    ado

--- ADO01/frmADO01.Designer.cs
+++ ADO01/frmADO01.Designer.cs
@@ -28,13 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.datagwCustomers = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.datagwCustomers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // datagwCustomers
+            // 
+            this.datagwCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datagwCustomers.Location = new System.Drawing.Point(49, 53);
+            this.datagwCustomers.Name = "datagwCustomers";
+            this.datagwCustomers.Size = new System.Drawing.Size(497, 226);
+            this.datagwCustomers.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(464, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Çıkış";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(45, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Customers Data";
+            // 
+            // frmADO01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(567, 363);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.datagwCustomers);
+            this.Name = "frmADO01";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DataADO.NET";
+            this.Load += new System.EventHandler(this.frmADO01_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.datagwCustomers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView datagwCustomers;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ADO01/frmADO01.cs
+++ ADO01/frmADO01.cs
@@ -10,11 +10,36 @@ using System.Windows.Forms;
 
 namespace ADO01
 {
+
     public partial class frmADO01 : Form
     {
+        // Global kısım
+        // Aşağıdaki değişken Veritabanına bağlanabilmek için gerekli olan bağlantı cümleciğidir.Şu makinaya ..şu databaseden bağlanmak istiyorum
+
+        string constring = @"Data Source=DESKTOP-V653CLI\SQLEXPRESS01;Initial Catalog=Northwind;Integrated Security=True"; // slash varsa string gibi anlaması için başına "@" koyuyoruz
+
         public frmADO01()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void frmADO01_Load(object sender, EventArgs e)
+        {
+            //form ilk ekrana geldiğinde DataGrid otomatik dolu gelsin.
+            PrepareGrid(); // DataGridimi hazırlıyorum...
+        }
+
+        // 
+        private void PrepareGrid()
+        {
+            datagwCustomers.AutoGenerateColumns = false; // datagrid in otomatik olarak database tablosuna bakıp gridin kolonlarını yaratmasını istemiyorum. Manuel
+
+
         }
     }
 }
