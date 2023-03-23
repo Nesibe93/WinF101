fcc933bf72e5c0e1a97a932fe7ad31bb4b2220e1
Nesibe93 <125722258+Nesibe93@users.noreply.github.com>
2023-03-23 12:58:01 +0300

    ...

--- ADO01/frmProducts.Designer.cs
+++ ADO01/frmProducts.Designer.cs
@@ -31,70 +31,74 @@
             this.btnClose = new System.Windows.Forms.Button();
             this.tabcProducts = new System.Windows.Forms.TabControl();
             this.tabpData = new System.Windows.Forms.TabPage();
-            this.tabpDetail = new System.Windows.Forms.TabPage();
-            this.grpbQuery = new System.Windows.Forms.GroupBox();
             this.datagwProducts = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtQProductName = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.cmbQCategory = new System.Windows.Forms.ComboBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.cmbQSupplier = new System.Windows.Forms.ComboBox();
+            this.grpbQuery = new System.Windows.Forms.GroupBox();
             this.btnQuery = new System.Windows.Forms.Button();
+            this.cmbQSupplier = new System.Windows.Forms.ComboBox();
+            this.cmbQCategory = new System.Windows.Forms.ComboBox();
+            this.txtQProductName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabpDetail = new System.Windows.Forms.TabPage();
             this.tabcProducts.SuspendLayout();
             this.tabpData.SuspendLayout();
-            this.grpbQuery.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.datagwProducts)).BeginInit();
+            this.grpbQuery.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnClose
             // 
+            this.btnClose.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.btnClose.Font = new System.Drawing.Font("Cascadia Code SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnClose.ForeColor = System.Drawing.Color.Red;
-            this.btnClose.Location = new System.Drawing.Point(660, 489);
+            this.btnClose.Location = new System.Drawing.Point(812, 483);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(81, 28);
             this.btnClose.TabIndex = 0;
             this.btnClose.Text = "Kapat";
-            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.UseVisualStyleBackColor = false;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // tabcProducts
             // 
             this.tabcProducts.Controls.Add(this.tabpData);
             this.tabcProducts.Controls.Add(this.tabpDetail);
-            this.tabcProducts.Font = new System.Drawing.Font("Cascadia Code SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tabcProducts.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.tabcProducts.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.tabcProducts.Location = new System.Drawing.Point(12, 12);
             this.tabcProducts.Name = "tabcProducts";
             this.tabcProducts.SelectedIndex = 0;
-            this.tabcProducts.Size = new System.Drawing.Size(733, 475);
+            this.tabcProducts.Size = new System.Drawing.Size(881, 471);
+            this.tabcProducts.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
             this.tabcProducts.TabIndex = 1;
+            this.tabcProducts.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.tabcProducts_DrawItem);
             // 
             // tabpData
             // 
+            this.tabpData.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.tabpData.Controls.Add(this.datagwProducts);
             this.tabpData.Controls.Add(this.grpbQuery);
-            this.tabpData.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.tabpData.ForeColor = System.Drawing.Color.Blue;
             this.tabpData.Location = new System.Drawing.Point(4, 26);
             this.tabpData.Name = "tabpData";
             this.tabpData.Padding = new System.Windows.Forms.Padding(3);
-            this.tabpData.Size = new System.Drawing.Size(725, 445);
+            this.tabpData.Size = new System.Drawing.Size(873, 441);
             this.tabpData.TabIndex = 0;
             this.tabpData.Text = "General";
             this.tabpData.UseVisualStyleBackColor = true;
             // 
-            // tabpDetail
+            // datagwProducts
             // 
-            this.tabpDetail.Location = new System.Drawing.Point(4, 26);
-            this.tabpDetail.Name = "tabpDetail";
-            this.tabpDetail.Padding = new System.Windows.Forms.Padding(3);
-            this.tabpDetail.Size = new System.Drawing.Size(725, 339);
-            this.tabpDetail.TabIndex = 1;
-            this.tabpDetail.Text = "Detail";
-            this.tabpDetail.UseVisualStyleBackColor = true;
+            this.datagwProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datagwProducts.Location = new System.Drawing.Point(6, 183);
+            this.datagwProducts.Name = "datagwProducts";
+            this.datagwProducts.Size = new System.Drawing.Size(861, 256);
+            this.datagwProducts.TabIndex = 1;
             // 
             // grpbQuery
             // 
+            this.grpbQuery.BackColor = System.Drawing.Color.Silver;
             this.grpbQuery.Controls.Add(this.btnQuery);
             this.grpbQuery.Controls.Add(this.cmbQSupplier);
             this.grpbQuery.Controls.Add(this.cmbQCategory);
@@ -102,48 +106,34 @@
             this.grpbQuery.Controls.Add(this.label3);
             this.grpbQuery.Controls.Add(this.label2);
             this.grpbQuery.Controls.Add(this.label1);
-            this.grpbQuery.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.grpbQuery.ForeColor = System.Drawing.SystemColors.Highlight;
             this.grpbQuery.Location = new System.Drawing.Point(6, 6);
             this.grpbQuery.Name = "grpbQuery";
-            this.grpbQuery.Size = new System.Drawing.Size(713, 171);
+            this.grpbQuery.Size = new System.Drawing.Size(861, 171);
             this.grpbQuery.TabIndex = 0;
             this.grpbQuery.TabStop = false;
             this.grpbQuery.Text = "Sorgu Seçenekleri";
             // 
-            // datagwProducts
+            // btnQuery
             // 
-            this.datagwProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.datagwProducts.Location = new System.Drawing.Point(6, 183);
-            this.datagwProducts.Name = "datagwProducts";
-            this.datagwProducts.Size = new System.Drawing.Size(713, 256);
-            this.datagwProducts.TabIndex = 1;
+            this.btnQuery.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnQuery.ForeColor = System.Drawing.Color.Black;
+            this.btnQuery.Location = new System.Drawing.Point(9, 135);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(76, 30);
+            this.btnQuery.TabIndex = 3;
+            this.btnQuery.Text = "Sorgula";
+            this.btnQuery.UseVisualStyleBackColor = false;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
             // 
-            // label1
+            // cmbQSupplier
             // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.label1.Location = new System.Drawing.Point(0, 33);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Ürün Adı : ";
-            // 
-            // txtQProductName
-            // 
-            this.txtQProductName.Location = new System.Drawing.Point(92, 30);
-            this.txtQProductName.Name = "txtQProductName";
-            this.txtQProductName.Size = new System.Drawing.Size(196, 23);
-            this.txtQProductName.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.label2.Location = new System.Drawing.Point(0, 62);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 17);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Kategori : ";
+            this.cmbQSupplier.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbQSupplier.FormattingEnabled = true;
+            this.cmbQSupplier.Location = new System.Drawing.Point(92, 90);
+            this.cmbQSupplier.Name = "cmbQSupplier";
+            this.cmbQSupplier.Size = new System.Drawing.Size(196, 25);
+            this.cmbQSupplier.TabIndex = 2;
             // 
             // cmbQCategory
             // 
@@ -153,6 +143,13 @@
             this.cmbQCategory.Name = "cmbQCategory";
             this.cmbQCategory.Size = new System.Drawing.Size(133, 25);
             this.cmbQCategory.TabIndex = 2;
+            // 
+            // txtQProductName
+            // 
+            this.txtQProductName.Location = new System.Drawing.Point(92, 30);
+            this.txtQProductName.Name = "txtQProductName";
+            this.txtQProductName.Size = new System.Drawing.Size(196, 23);
+            this.txtQProductName.TabIndex = 1;
             // 
             // label3
             // 
@@ -164,40 +161,52 @@
             this.label3.TabIndex = 0;
             this.label3.Text = "Tedarikçi: ";
             // 
-            // cmbQSupplier
+            // label2
             // 
-            this.cmbQSupplier.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbQSupplier.FormattingEnabled = true;
-            this.cmbQSupplier.Location = new System.Drawing.Point(92, 90);
-            this.cmbQSupplier.Name = "cmbQSupplier";
-            this.cmbQSupplier.Size = new System.Drawing.Size(196, 25);
-            this.cmbQSupplier.TabIndex = 2;
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Location = new System.Drawing.Point(0, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Kategori : ";
             // 
-            // btnQuery
+            // label1
             // 
-            this.btnQuery.ForeColor = System.Drawing.Color.Black;
-            this.btnQuery.Location = new System.Drawing.Point(9, 135);
-            this.btnQuery.Name = "btnQuery";
-            this.btnQuery.Size = new System.Drawing.Size(76, 30);
-            this.btnQuery.TabIndex = 3;
-            this.btnQuery.Text = "Sorgula";
-            this.btnQuery.UseVisualStyleBackColor = true;
-            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(0, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ürün Adı : ";
+            // 
+            // tabpDetail
+            // 
+            this.tabpDetail.Location = new System.Drawing.Point(4, 26);
+            this.tabpDetail.Name = "tabpDetail";
+            this.tabpDetail.Padding = new System.Windows.Forms.Padding(3);
+            this.tabpDetail.Size = new System.Drawing.Size(873, 441);
+            this.tabpDetail.TabIndex = 1;
+            this.tabpDetail.Text = "Detail";
+            this.tabpDetail.UseVisualStyleBackColor = true;
             // 
             // frmProducts
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(755, 546);
+            this.ClientSize = new System.Drawing.Size(921, 561);
             this.Controls.Add(this.tabcProducts);
             this.Controls.Add(this.btnClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "frmProducts";
             this.Text = "frmProducts";
+            this.Load += new System.EventHandler(this.frmProducts_Load);
             this.tabcProducts.ResumeLayout(false);
             this.tabpData.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.datagwProducts)).EndInit();
             this.grpbQuery.ResumeLayout(false);
             this.grpbQuery.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.datagwProducts)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -206,7 +215,6 @@
 
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.TabControl tabcProducts;
-        private System.Windows.Forms.TabPage tabpData;
         private System.Windows.Forms.DataGridView datagwProducts;
         private System.Windows.Forms.GroupBox grpbQuery;
         private System.Windows.Forms.TabPage tabpDetail;
@@ -217,5 +225,6 @@
         private System.Windows.Forms.ComboBox cmbQSupplier;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnQuery;
+        public System.Windows.Forms.TabPage tabpData;
     }
 }
--- ADO01/frmProducts.cs
+++ ADO01/frmProducts.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -29,9 +30,150 @@ namespace ADO01
             this.Close();
         }
 
+        private void PrepareGrid()
+        {
+            datagwProducts.AutoGenerateColumns = true; // datagrid in otomatik olarak database tablosuna bakıp gridin kolonlarını yaratmasını istemiyorum. Manuel
+            // Eğer DataGridin en baş taraftaki kolonu 'RowHeader' görmek istemiyorsam
+
+            datagwProducts.RowHeadersVisible = false;
+
+            // DatagRiddeki kolonları tek tek dolaşmak yerine tek bir satırı ayrıştırma
+            datagwProducts.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            // DataGriddeki kolonları DataGridin genişliğine göre ayarlama
+            datagwProducts.AutoSizeColumnsMode =
+                DataGridViewAutoSizeColumnsMode.Fill;
+
+            // DataGridin üzerinde elle oynamayı yasaklama 
+            datagwProducts.ReadOnly = true;
+
+            // DataGridin üzerinde kullanıcının satır eklemesini ve silmesini yasaklama
+            datagwProducts.AllowUserToAddRows = false;
+            datagwProducts.AllowUserToDeleteRows = false;
+
+            // DataGridin satırlarının yüksekliğini ve genişliğini yasaklama
+            datagwProducts.AllowUserToResizeRows = false;
+        }
+
+        private void BindGrid()
+        {
+            // DataGrid i dolduran bölüm
+            // C# ın using kalıbı. ...bunu kullanarak demek
+
+            using (SqlConnection con = new SqlConnection(constring)) // con isminde bir nesne yarat(SqlConnection kütüphanesinden). yaratırken de constringden yararlan.
+            {
+                #region SQL Sorgu
+                //                SELECT ProductID, ProductName, Suppliers.CompanyName,Categories.CategoryName,UnitsInStock,Discontinued FROM Products
+                //INNER JOIN Suppliers ON Products.SupplierID = Suppliers.SupplierID
+                //INNER JOIN Categories ON Products.CategoryID = Categories.CategoryID
+                //WHERE ProductID > 0
+                #endregion
+
+                vs_SQLCommand = "SELECT ProductID, ProductName, Suppliers.CompanyName,Categories.CategoryName,UnitsInStock,Discontinued FROM Products ";
+                vs_SQLCommand = vs_SQLCommand + "INNER JOIN Suppliers ON Products.SupplierID = Suppliers.SupplierID ";
+                vs_SQLCommand += "INNER JOIN Categories ON Products.CategoryID = Categories.CategoryID ";
+                   vs_SQLCommand += "WHERE ProductID > 0";
+                
+
+                using (SqlCommand cmd = new SqlCommand(vs_SQLCommand, con)) // con nesnesini kullarak SQL komutunu oluştur
+                {
+                    // aşağıda cmd nesnesini kullanabileceğim veri geliş/gidişini ayarlayan bir DataAdapter oluşturuyoruz
+                    cmd.CommandType = CommandType.Text; // text şeklinde
+
+                    using (SqlDataAdapter sqlda = new SqlDataAdapter(cmd))
+                    {
+                        using (DataSet dataSet = new DataSet())
+                        {
+                            sqlda.Fill(dataSet); // Adaptörüm yarattığı dataSeti doldursun
+                            datagwProducts.DataSource = dataSet.Tables[0]; // dataSet oluştu..içine table taşındı ve DataGridin içinde görülebilir hale geldi
+                        }
+                    }
+                }
+            }
+        }
+
+        private void frmProducts_Load(object sender, EventArgs e)
+        {
+            PrepareGrid();
+            BindGrid();
+            GetCategory();
+        }
+
+        private void GetCategory()
+        {
+            // SQL tarafındaki Category Tablosundan sorgulamada kullanabilmek için sadece CategoryID ve CategoryName alanlarını almalıyım
+
+            vs_SQLCommand = "SELECT CategoryID,CategoryName FROM Categories";
+
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                using (SqlCommand cmd = new SqlCommand(vs_SQLCommand, con))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    using (SqlDataAdapter sqlda = new SqlDataAdapter(cmd))
+                    {
+                        using (DataSet dataSet = new DataSet())
+                        {
+                            sqlda.Fill(dataSet); // adaptörün yarattığı DataSeti doldursun
+
+                            // comboboxın ilk satırında -- Hepsi -- yazsın
+
+                            // data tablolarında olmayan bir satırı oluşturmak için kullanılan bir class var(DataRow classı) combo içine ilk olarak onun gözükmesini sağlıyacağım
+
+                            DataRow newRow = dataSet.Tables[0].NewRow();
+                            newRow = "";
+
+
+
+
+
+
+
+                            cmbQCategory.DataSource = dataSet.Tables[0];
+                            cmbQCategory.ValueMember = "CategoryID";
+                            cmbQCategory.DisplayMember = "CategoryName";
+                        }
+                      
+                    }
+                }
+            }
+        }
+
+        private void tabcProducts_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            // Sekmenin arka planını ve yazı rengini belirleyin
+            Brush backBrush = new SolidBrush(Color.LightBlue);
+            Brush foreBrush = new SolidBrush(Color.Black);
+
+            // Sekme başlığı metnini alın
+            string tabText = this.tabcProducts.TabPages[e.Index].Text;
+
+           
+            // Metnin boyutunu alın
+            SizeF textSize = e.Graphics.MeasureString(tabText, this.Font);
+
+            // Metni çizin
+            e.Graphics.FillRectangle(backBrush, e.Bounds);
+            e.Graphics.DrawString(tabText, this.Font, foreBrush,
+                e.Bounds.Left + (e.Bounds.Width - textSize.Width) / 2,
+                e.Bounds.Top + (e.Bounds.Height - textSize.Height) / 2);
+
+            // Sekmenin başlığı fontunu kalın olarak ayarlayın
+            Font tabFont = new Font(this.Font, FontStyle.Bold);
+            //tabFont = this.tabcProducts.Font;
+
+            // Fırçaları serbest bırakın
+            backBrush.Dispose();
+            foreBrush.Dispose();
+        }
+
         private void btnQuery_Click(object sender, EventArgs e)
         {
+            vs_SQLQuery = "";
+            if (txtQProductName.Text != "") // adam textbox a bir şey girmiş mi
+            {
 
+            }
         }
     }
 }
